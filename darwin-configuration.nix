3ab24e70cd634a947f4fa367369ab1ad728dbccc
Brian Ginsburg <gins@brianginsburg.com>
2024-05-17 14:25:00 -0700

    Remove unused substitutors

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -20,16 +20,16 @@
     package = pkgs.nix;
     settings = {
       trusted-users = ["root" "@wheel"];
-      trusted-substituters = [
-        "https://cache.nixos.org"
-        "https://hydra.iohk.io"
-        "https://static-haskell-nix.cachix.org"
-      ];
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-        "static-haskell-nix.cachix.org-1:Q17HawmAwaM1/BfIxaEDKAxwTOyRVhPG5Ji9K3+FvUU="
-      ];
+      # trusted-substituters = [
+      #   "https://cache.nixos.org"
+      #   "https://hydra.iohk.io"
+      #   "https://static-haskell-nix.cachix.org"
+      # ];
+      # trusted-public-keys = [
+      #   "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      #   "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+      #   "static-haskell-nix.cachix.org-1:Q17HawmAwaM1/BfIxaEDKAxwTOyRVhPG5Ji9K3+FvUU="
+      # ];
     };
 
     extraOptions = ''
